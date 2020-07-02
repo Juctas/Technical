0a2eb787b7d2a04d0672e5281225eb3f1c7f2174
Juctas <49431179+Juctas@users.noreply.github.com>
2020-07-02 11:03:44 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Line from './components/Line'
+
+function App() {
+  return (
+    <div className="App">
+      <Line heights={10}/>
+    </div>
+  );
+}
+
+export default App;
